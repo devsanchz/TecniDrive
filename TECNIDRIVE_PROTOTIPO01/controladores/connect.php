de7c7e7552be5810494b60efae2fed87347c03a5
Snnake Cruz <sanchezguzmndavid@gmail.com>
2026-05-15 06:48:40 -0500

    cambios de orden
    docs: reorganize en una estructura MVC lo archivos

--- TECNIDRIVE_PROTOTIPO01/controladores/connect.php
+++ TECNIDRIVE_PROTOTIPO01/controladores/connect.php
@@ -2,5 +2,14 @@
 
 $user = "root";
 $host = "localhost";
-$dbname = "";
+$dbname = "tecnidrive_03";
+
+
+try{
+    $sql = "mysql: host:$host; dbname=$dbname; user=$user; charset=utf8";
+
+}
+catch(exception $e){
+    echo"error en la conexion".$e -> GetMessage();
+}
 ?>
